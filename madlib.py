bc5384d5ecd58506649b3a078dd953090afe0917
Scott Currie <vitriolik@gmail.com>
2018-11-28 17:58:34 -0800

    Expanded functionality

--- madlib.py
+++ madlib.py
@@ -1,8 +1,9 @@
 import re
 
+
 def print_intro():
     """Show intro and instructions to user."""
-    print('Let\'s play madlibs. I\'ll ask you to provide some words based on a prompt. When you\'ve given all your answers, I\'ll tell you a story.)
+    print('Let\'s play madlibs. I\'ll ask you to provide some words based on a prompt. When you\'ve given all your answers, I\'ll tell you a story.')
 
 
 def get_template_text(template_filename):
@@ -13,36 +14,38 @@ def get_template_text(template_filename):
 
 def parse_template_text(template_text):
     """Parse the template text. Return a list of user prompts and the template text with the prompts stripped out."""
-    pattern = '/\{(.*?)\}/g'
-    return re.findall(pattern, template_text)
+    pattern = '\{(.*?)\}'
+    prompts = re.findall(pattern, template_text)
+    template_text = re.sub(pattern, '{}', template_text)
+    return template_text, prompts
 
 
 def get_user_responses(prompts):
     """Go through the list of prompts and reqcord user's answers. Return the answers in a list."""
     responses = []
     for prompt in prompts:
-        responses.append(input(prompt))
+        responses.append(input(f'Enter a {prompt.lower()}: '))
     return responses
 
 
-def format_template_text(template_text, prompts):
+def format_template_text(template_text, responses):
     """Replace the text blanks with the user's responses. Return the string."""
-    pass
+    return template_text.format(*responses)
 
 
 def print_formatted_text(template_text):
     """Print out the final text."""
-    pass
+    print(template_text)
 
 
 def run():
     """Enter script execution."""
-    show_intro()
+    print_intro()
     filename = 'template.txt'
     template_text = get_template_text(filename)
-    prompts, template_text = parse_template_text(template_text)
+    template_text, prompts = parse_template_text(template_text)
     responses = get_user_responses(prompts)
-    template_text = format_template_text(template_text, prompts)
+    template_text = format_template_text(template_text, responses)
     print_formatted_text(template_text)
 
 
